f0daed52cac1fc64e0a9f5e75563e50da954a0e0
demunnib <54548702+demunnib@users.noreply.github.com>
2019-09-05 18:17:14 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stats_lib.h"
+#include <math.h>
+int main()
+{
+    int i;
+      FILE *fp;
+      int count = 143;
+   fp = fopen("data.txt", "r");float nums[count];
+
+    if (fp == NULL)
+    {
+        printf("Error Reading File\n");
+    }
+     for(i=0; i<143; i++)
+     {
+         fscanf(fp, "%f", &nums[i]);
+     }
+     float max = maximum(nums, count);
+     float min = minimum(nums, count);
+     float avg = mean(nums, count);
+     float mead = median(nums, count);
+     float var = variance(nums, count);
+     float stddev = standard_deviation(nums, count);
+
+printf("max: %.0f\nmin: %.0f\nmean:%.0f\nmedian:%.0f\nvariance:%.0f\nstandard deviation:%.0f",max ,min, avg, mead, var, stddev);
+fclose(fp);
+}
